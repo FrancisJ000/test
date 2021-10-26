58a3b24780ee492673d06201c228a894a8b451c9
Allen Joseph <allenxavier45@gmail.com>
2021-10-26 15:50:20 +0530

    add server.js

--- Jquery/index.js
+++ Jquery/index.js
@@ -1,1 +1,16 @@
-$("h1").css("color","red")
+const { response } = require('express');
+const express = require('express');
+
+const app = express();
+
+app.get("/",function(request,response){
+    response.send("HEY");
+})
+
+app.get("/contact",function(request,response){
+    response.send("no");
+})
+
+app.listen(3000,function(){
+    console.log("server started: 3000");
+})
